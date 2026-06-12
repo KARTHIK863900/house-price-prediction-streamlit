bb644295d65f4aafa75ad884523bc9763b1ab61e
KARTHIK863900 <karthikyanamala85@gmail.com>
2026-06-12 08:47:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import pickle
+import streamlit as st
 import pandas as pd
+from pickle import load
+st.write('Regression Model')
+st.write('How much House-Price')
+def get_input():
+    bul=st.sidebar.number_input('Enter the Buliding type',min_value=1,max_value=2)
+    yy=st.sidebar.number_input('Enter the year of sale',min_value=2000,max_value=2025)
+    mm=st.sidebar.slider('Enter the month of sale',min_value=1,max_value=12)
+    typp=st.sidebar.selectbox('Enter the type of property',['House','Apartment'])
+    pp=st.sidebar.number_input('Enter the property')
+    Area=st.sidebar.number_input('Enter the area in ft')
+    status=st.sidebar.selectbox('status',['Available','sold'])
+    if typp=='House':
+       typp=0
+    else:
+        typp=1
+    if status=='Available':
+        status=1
+    else:
+        status=0
+    user_input=pd.DataFrame({
+        'Building Type':bul,
+        'Year of sale':yy,
+        'Month of sale':mm,
+        'Type of property':typp,
+        'Property #':pp,
+        'Area (ft.)':Area,
+        'Status':status},index=[1])
+    return user_input
+features=get_input()
+loaded_model=load(open('lreg.pkl','rb'))
+if st.sidebar.button('Submit'):
+    st.write(features)
+    res=loaded_model.predict(features)
+    st.success(f"The House Price is:{res[0]:,.2f}")
 
-app = FastAPI()
 
-model = pickle.load(open("lreg.pkl", "rb"))
-
-class InputData(BaseModel):
-    Area: float
-    Room: int
-    Parking: int
-    Warehouse: int
-    Elevator: int
-
-@app.get("/")
-def home():
-    return {"message": "House Price Prediction API"}
-
-@app.post("/predict")
-def predict(data: InputData):
-
-    df = pd.DataFrame({
-        "Area":[data.Area],
-        "Room":[data.Room],
-        "Parking":[data.Parking],
-        "Warehouse":[data.Warehouse],
-        "Elevator":[data.Elevator]
-    })
-
-    prediction = model.predict(df)
-
-    return {
-        "Predicted Price": float(prediction[0])
-    }
